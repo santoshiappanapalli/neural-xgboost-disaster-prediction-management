91844e7f7d1582c5b9a4f4366793d686f159e2e2
santoshiappanapalli <157392342+santoshiappanapalli@users.noreply.github.com>
2026-08-05 18:58:58 +0530

    Add files via upload

---
+++ admins/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class AdminOTP(models.Model):
+    email = models.EmailField()
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.email} - {self.otp}"
+
